208f440cf8af4761326b06ec09c5c6c7ffc6de75
adarshreddyash <adarshreddy9849@gmail.com>
2019-01-24 22:03:04 +0530

    initial commit

---
+++ Finite_automata/DFA.py
@@ -0,0 +1,12 @@
+dfa = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+
+def accepts(transitions,initial,accepting,s):
+    state = initial
+    for c in s:
+        state = transitions[state][c]
+    return state in accepting
+
+print(accepts(dfa,0,{0},'1011101'))
+print(accepts(dfa,0,{0},'1011111'))
